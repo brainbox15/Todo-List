f55856c7311243f82c7ce6d5714eaf2634c2295e
MICK <omorakafearless@yahoo.com>
2022-03-13 09:25:31 -0700

    added a md file

--- todo.js
+++ todo.js
@@ -1,12 +1,18 @@
-
+// grabs and stores #container-form
 var containerForm = document.querySelector('#container-form')
+
+// attaches eventlistener to containerForm
 containerForm.addEventListener('submit', addItem)
 list = document.querySelector('#list')
 
+// grabs and stores #header-input
 headerInput = document.querySelector('#header-input')
+
+// attaches eventlistener to headerInput
 headerInput.addEventListener('keyup', filterItems)
 
-function addItem(e){
+// To add list items
+function addItem(e) {
     e.preventDefault()
     textInput = document.querySelector('#text-input').value
     // creating a new list
@@ -16,23 +22,25 @@ function addItem(e){
     list.appendChild(li)
 
 
-//    creating a new button
-button = document.createElement('button')
-button.setAttribute('class', 'delete')
-button.innerText= 'x'
-li.appendChild(button)
+    //    creating a new button
+    button = document.createElement('button')
+    i = document.createElement('i')
+    i.setAttribute('class', 'delete fas fa-trash-alt')
+    button.appendChild(i)
+    li.appendChild(button)
 
 }
 
 
 list.addEventListener('click', deleteItems)
 
-function deleteItems(e){
 
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are you sure')){
-        li = e.target.parentElement
-        list.removeChild(li)
+// Deletes list items
+function deleteItems(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure')) {
+            li = e.target.parentElement.parentElement
+            list.removeChild(li)
         }
     }
 
@@ -40,22 +48,19 @@ function deleteItems(e){
 }
 
 
-
-
-function filterItems(e){
+// filters list items
+function filterItems(e) {
 
     // convert text to lowercase
-text =e.target.value.toLowerCase()
-  items= list.getElementsByTagName('li')
-console.log(items)
-  Array.from(items).forEach(function(item){
-       itemName = item.firstChild.textContent
-      if(itemName.toLowerCase().indexOf(text) != -1){
-          item.style.display = 'flex'
-      }
-
-      else{
-          item.style.display = 'none'
-      }
-  })
+    text = e.target.value.toLowerCase()
+    items = list.getElementsByTagName('li')
+    console.log(items)
+    Array.from(items).forEach(function (item) {
+        itemName = item.firstChild.textContent
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'flex'
+        } else {
+            item.style.display = 'none'
+        }
+    })
 }
---
+++ todoList.md
@@ -0,0 +1,46 @@
+# To-Do List
+ A simple Manager for Daily Tasks.
+
+> Live demo, click [_here_](https://task360.netlify.app). 
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Screenshots](#screenshots)
+* [Setup](#setup)
+* [Project Status](#project-status)
+
+
+## General Information
+ A simple task Manager, that enables a user to maintain day-to-day tasks or list everything that he/she have to do. Built using HTML5, CSS3 and Vanilla JS
+
+
+## Technologies Used
+- HTML5
+- CSS3 
+- JAVASCRIPT 
+
+
+## Features
+In this version of To-Do List, the user will be getting four options:
+- Create (add) a new task or adding a new To-Do 
+- View all the To-Dos thar were added.
+- Delete any To-Do from the list of To-Dos with confirmation box to proceed.
+- Filter the added To-Dos.
+
+## Screenshots
+![Example screenshot](images/todoList.png)
+
+
+
+## Setup
+To build this project, the following are required:
+- Text editor: This is needed to write your code. You can use any text editor of your choice. In this case, i used VSCode. click [_here_](https://code.visualstudio.com/Download) to download 
+
+- Browser: You can use any brower of your choice, e.g: Chrome, Firefox etc. it is always advisable to test your program across multiple browers to check compatibility.
+
+## Project Status
+Project is: _complete_
+
+
